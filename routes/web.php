03777e0d2a887f8d56e6a51557b371a0d3c2940f
Michael Norris <hello@michaelnorris.co.uk>
2017-10-24 23:03:51 +0100

    can request tokens

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://passport.dev/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'password',
+            'client_id' => '2',
+            'client_secret' => 'Xc8ZO7kCqePQkCg6Zc8sKwV7RuNIxB3rqDmS5Dof',
+            'username' => 'mstnorris@gmail.com',
+            'password' => 'secret',
+            'scope' => '*',
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
 });
